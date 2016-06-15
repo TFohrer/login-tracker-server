d874c203a2de055970b24f9d285c67186fa6ede8
tfohrer <tarek.fohrer@umwerk.net>
2016-06-15 23:34:39 +0200

    initial node rest api setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # login-tracker-server
+
+# Install
+run "npm install"
+
+
+# Start
+run "node app.js"
--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-/**
- * Created by tfohrer on 15.06.16.
- */
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var routes = require("./routes/routes.js")(app);
+
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
